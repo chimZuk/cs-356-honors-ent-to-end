1e52c45785c5b745b8aa1f9bcfa68e596d1363cd
DZMITRY KUZMITCH <kuzya19989@outlook.com>
2019-12-01 23:13:04 -0500

    Added drag library

--- js/script.js
+++ js/script.js
@@ -1,1 +1,33 @@
-console.log("Kek");
+interact('.draggable')
+    .draggable({
+        inertia: true,
+        modifiers: [
+            interact.modifiers.restrictRect({
+                restriction: 'parent',
+                endOnly: true
+            })
+        ],
+        autoScroll: true,
+        onmove: on_drag_move,
+        onend: on_drag_end
+    })
+
+function on_drag_move(event) {
+    var target = event.target;
+    var x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx;
+    var y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
+
+    target.style.webkitTransform =
+        target.style.transform =
+        'translate(' + x + 'px, ' + y + 'px)';
+
+    target.setAttribute('data-x', x);
+    target.setAttribute('data-y', y);
+}
+
+function on_drag_end(event) {
+    console.log('moved a distance of ' +
+        (Math.sqrt(Math.pow(event.pageX - event.x0, 2) +
+            Math.pow(event.pageY - event.y0, 2) | 0))
+            .toFixed(2) + 'px');
+}
